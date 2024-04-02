569234c906000a9955fb8c25036c663968f44c58
Med-Boff <boafiamohamed@gmail.com>
2024-04-02 22:12:45 +0000

    modifiying first task file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
     n_elements = 0
     try:
         for i in range(x):
-            print(my_list[i], end=" ")
+            print(my_list[i], end=' ')
             n_elements += 1
     except IndexError:
         pass
