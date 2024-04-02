b99a0b75dde6a0eed2dbe22379c074424efbc36a
Med-Boff <boafiamohamed@gmail.com>
2024-04-02 22:03:46 +0000

    adding first task file

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+def safe_print_list(my_list=[], x=0):
+    n_elements = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end=" ")
+            n_elements += 1
+    except IndexError:
+        pass
+    print()
+    return n_elements
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+this directory python exceptions project files
